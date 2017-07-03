26be954c01ebe175920cb18fa959b8e6876b3afe
Karthikaveni <karthikaveni18@gmail.com>
2017-07-03 20:51:23 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+# POSITIVE-OR-NEGATIVE
+import java.io.*;
+import java.util.*;
+public class Main{
+  public static void main(String args[])throws IOException
+  {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    int a;
+    a=Integer.parseInt(br.readLine());
+    if(a==0)
+    System.out.println("Zero");
+    else if(a>0)
+    System.out.println("positive");
+    else
+    System.out.println("negative");
+  }
+}
